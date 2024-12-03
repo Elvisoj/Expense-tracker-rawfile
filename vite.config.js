48260b89917ef727294f24ab7a4772b4b0fba24d
Elvis Jonathan <ighoelvis8@gmail.com>
2024-12-03 18:47:09 +0100

    now

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  bash: "/Expense-tracker-rawfile/"
+  bash: "/Expense-tracker-rawfile/",
 })
